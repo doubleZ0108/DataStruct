ee271f60fd2c62c6fbb8a88160bcbf6a979ef970
double Z <39005525+doubleZ0108@users.noreply.github.com>
2018-10-26 21:49:41 +0800

    回溯法解迷宫问题(初稿)

--- Maze.cpp
+++ Maze.cpp
@@ -3,8 +3,10 @@
 #include <vector>
 #include <list>
 using namespace std;
-#define ACC 0
-#define OBS 1
+
+#define ACC 0		//ACC代表这个位置是通路
+#define OBS 1		//OBS为障碍物
+
 struct Point
 {
 	int x, y;
@@ -14,40 +16,124 @@ struct Point
 };
 struct Offsets
 {
-	Point pos;
-	const char *dir;
-
+	Point pos;			//方向向量
+	const char *dir;	//方向名字
 };
 Offsets Move[8] = { { Point(-1,0),"N"},{ Point(-1,1),"NE"},{ Point(0,1),"E"},{ Point(1,1),"SE"},
 					{ Point(1,0),"S"},{ Point(1,-1),"SW"},{ Point(0,-1),"W"},{ Point(-1,-1),"NW"} };
+	//八个方位的向量
 
 bool SeekPath(Point now, const Point dest, 
 	vector<vector<int> > &Maze, vector<vector<int> > &mark, list<Point> &AccessLink)
+	//当前位置 终点位置 整张地图 记录哪些点走过了的地图 结果链表用于记录每一步的坐标
 {
-	Offsets buf;
+	Offsets buf;	//假设往这个方向走的替代变量
 
-	if (now == dest) { return true; }
+	if (now == dest) { return true; }	//如果找到终点了就退出递归并返回true
 
 	for (int i = 0; i < 8; ++i)
+		//8个方向都尝试一遍能不能走
 	{
 		buf.pos.x = now.x + Move[i].pos.x;
 		buf.pos.y = now.y + Move[i].pos.y;
 		buf.dir = Move[i].dir;
 
+		if (buf.pos.x < 0 || buf.pos.y < 0) { continue; }	//如果试探的走出了地图则直接换方向
+
 		if (Maze[buf.pos.x][buf.pos.y] == ACC && mark[buf.pos.x][buf.pos.y] == 0)
+			//如果地图上这个点可以走 && 以前没往这尝试过
 		{
-			mark[buf.pos.x][buf.pos.y] = 1;
+			mark[buf.pos.x][buf.pos.y] = 1;		//往这尝试的时候把mark地图修改一下
 
 			if (SeekPath(buf.pos, dest, Maze, mark, AccessLink))
 			{
-				AccessLink.push_front(buf.pos);
-				Maze[buf.pos.x][buf.pos.y] = 5;
+				AccessLink.push_front(buf.pos);		//因为是在递归之后的语句所以会逆序进行
+								//因此这里选择了往链表的头上插入,人工把结果再逆一遍就正了
+				Maze[buf.pos.x][buf.pos.y] = 5;		//地图上显示走的路径
 				return true;
 			}
 		}
 	}
 
 	return false;
+}
+
+void FindGap(int row, int col, const vector<vector<int> > &Maze, 
+	Point &start, Point &dest)
+//在地图上找出发点和终点的位置坐标
+//做法是在地图的四周找唯一的两个ACC, 然后随机赋值给出发点和终点
+//默认是四周有且仅有两个ACC
+{
+	int i, j;
+	int cnt = 0;
+	//上
+	for (int j = 0; j < col; ++j)
+	{
+		if (Maze[0][j] == ACC)
+		{
+			++cnt;
+			if (cnt == 1)
+			{
+				start = Point(0, j);
+			}
+			else if (cnt == 2)
+			{
+				dest = Point(0, j);
+				return;
+			}
+		}
+	}
+	//下
+	for (j = 0; j < col; ++j)
+	{
+		if (Maze[row - 1][j] == ACC)
+		{
+			++cnt;
+			if (cnt == 1)
+			{
+				start = Point(row-1, j);
+			}
+			else if (cnt == 2)
+			{
+				dest = Point(row-1, j);
+				return;
+			}
+		}
+	}
+	//左
+	for (i = 1; i < row - 1; ++i)
+	{
+		if (Maze[i][0] == ACC)
+		{
+			++cnt;
+			if (cnt == 1)
+			{
+				start = Point(i, 0);
+			}
+			else if (cnt == 2)
+			{
+				dest = Point(i, 0);
+				return;
+			}
+		}
+	}
+	//右
+	for (i = 1; i < row - 1; ++i)
+	{
+		if (Maze[i][col - 1] == ACC)
+		{
+			++cnt;
+			if (cnt == 1)
+			{
+				start = Point(i, col-1);
+			}
+			else if (cnt == 2)
+			{
+				dest = Point(i, col-1);
+				return;
+			}
+		}
+	}
 }
 
 int main(void)
@@ -66,21 +152,34 @@ int main(void)
 			cin >> Maze[i][j];
 		}
 	}
+	
+	//寻找出发点和终点坐标
+	Point start, dest;
+	FindGap(row, col, Maze, start, dest);
 
-	mark[1][0] = 1;
+	mark[start.x][start.y] = 1;		//出发点位置坐标
 	list<Point> AccessLink;
-	if (SeekPath(Point(1, 0), Point(row - 2, col - 1), Maze, mark, AccessLink))
+	if (SeekPath(start, dest, Maze, mark, AccessLink))
 	{
-		printf("(1, 0) dirction E\n\n");
-		Maze[1][0] = 5;
-	}
-
-	for (auto liter = AccessLink.begin(); liter != AccessLink.end(); ++liter)
-	{
-		printf("(%d, %d)->", (*liter).x, (*liter).y);
+		AccessLink.push_front(start);
+		Maze[start.x][start.y] = 5;
 	}
 
 
+	if (AccessLink.size() > 1)
+	{
+		Point save = AccessLink.back();
+		AccessLink.pop_back();
+		for (list<Point>::iterator liter = AccessLink.begin(); liter != AccessLink.end(); ++liter)
+		{
+			printf("(%d, %d)->", (*liter).x, (*liter).y);
+		}
+		printf("(%d, %d)", save.x, save.y);
+	}
+	else
+	{
+		cout << "此迷宫无解!\n";
+	}
 
 
 
@@ -101,6 +200,22 @@ int main(void)
 		cout << endl;
 	}
 
+	cout << endl << "**********************" << endl;
+	printf("   ");
+	for (int i = 0; i < col; ++i)
+	{
+		printf("%-3d", i);
+	}
+	cout << endl;
+	for (int i = 0; i < row; ++i)
+	{
+		printf("%-3d", i);
+		for (int j = 0; j < col; ++j)
+		{
+			printf("%-3d", mark[i][j]);
+		}
+		cout << endl;
+	}
 
 	system("pause");
 	return 0;
