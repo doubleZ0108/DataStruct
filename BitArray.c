74e282f053daf50515f71e806f7ea86fd52c795e
double Z <39005525+doubleZ0108@users.noreply.github.com>
2018-11-13 21:14:47 +0800

    比特数组的定义

---
+++ BitArray.c
@@ -0,0 +1,20 @@
+#include <limits.h>        /* for CHAR_BIT */
+#include <stdio.h>
+#include <string.h>
+#define MAX 10000
+
+//bit数组的定义
+#define BITMASK(b) (1 << ((b) % CHAR_BIT))
+#define BITSLOT(b) ((b) / CHAR_BIT)
+#define BITSET(a, b) ((a)[BITSLOT(b)] |= BITMASK(b))
+#define BITCLEAR(a, b) ((a)[BITSLOT(b)] &= ~BITMASK(b))
+#define BITTEST(a, b) ((a)[BITSLOT(b)] & BITMASK(b))
+#define BITNSLOTS(nb) ((nb + CHAR_BIT - 1) / CHAR_BIT)
+
+int main(void)
+{
+	
+
+	system("pause");
+	return 0;
+}
