fc31be74238897a335252add48ed6ec85f401274
double Z <39005525+doubleZ0108@users.noreply.github.com>
2018-12-17 20:00:52 +0800

    修理牧场（优先队列）

--- 修理牧场/RepairRanch.cpp
+++ 修理牧场/RepairRanch.cpp
@@ -7,13 +7,14 @@
 #include <algorithm>
 using namespace std;
 
-void RepairRanch_normal(vector<int> &woods, int n)
+//函数参数不能传reference，否则调用一个算法之后woods里就空了
+void RepairRanch_normal(vector<int> woods, int N)
 {
 	int cost = 0;
 	vector<int>::iterator bufiter;
 	int save;
 
-	for (int i = 0; i < n - 1; ++i)
+	for (int i = 0; i < N - 1; ++i)
 	{
 		save = 0;
 		bufiter = min_element(woods.begin(), woods.end());
@@ -29,6 +30,31 @@ void RepairRanch_normal(vector<int> &woods, int n)
 
 	cout << cost << endl;
 }
+void RepairRanch_preQueue(vector<int> woods, int N)
+{
+	int buf, cost = 0;
+	priority_queue<int, vector<int>, greater<int> > less_pre_queue;
+
+	for (int i = 0; i < N; ++i)
+	{
+		less_pre_queue.push(woods[i]);
+	}
+
+	int first, second;
+	for (int i = 0; i < N - 1; ++i)
+	{
+		first = less_pre_queue.top();
+		less_pre_queue.pop();
+		second = less_pre_queue.top();
+		less_pre_queue.pop();
+		less_pre_queue.push(first + second);
+
+		cost += first + second;
+	}
+
+	cout << cost << endl;
+}
+
 int main(void)
 {
 	int N;
@@ -39,7 +65,10 @@ int main(void)
 		cin >> woods[i];
 	}
 	
+	cout << "vector作为存储结构，每次人工找最小的：";
 	RepairRanch_normal(woods, woods.size());
+	cout << "优先队列：";
+	RepairRanch_preQueue(woods, woods.size());
 
 	system("pause");
 	return 0;
