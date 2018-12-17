811e13cda558634af759ae29e928519de95ba0d9
double Z <39005525+doubleZ0108@users.noreply.github.com>
2018-12-17 20:04:23 +0800

    优先队列 （终稿）

--- 修理牧场/RepairRanch.cpp
+++ 修理牧场/RepairRanch.cpp
@@ -17,12 +17,14 @@ void RepairRanch_normal(vector<int> woods, int N)
 	for (int i = 0; i < N - 1; ++i)
 	{
 		save = 0;
+
 		bufiter = min_element(woods.begin(), woods.end());
 		save += *bufiter;
 		woods.erase(bufiter, bufiter + 1);
 		bufiter = min_element(woods.begin(), woods.end());
 		save += *bufiter;
 		woods.erase(bufiter, bufiter + 1);
+
 		cost += save;
 
 		woods.push_back(save);
@@ -34,8 +36,10 @@ void RepairRanch_preQueue(vector<int> woods, int N)
 {
 	int buf, cost = 0;
 	priority_queue<int, vector<int>, greater<int> > less_pre_queue;
+			//使用优先队列存储
 
 	for (int i = 0; i < N; ++i)
+		//依次把各块木头的长度加入到优先队列中
 	{
 		less_pre_queue.push(woods[i]);
 	}
@@ -43,12 +47,16 @@ void RepairRanch_preQueue(vector<int> woods, int N)
 	int first, second;
 	for (int i = 0; i < N - 1; ++i)
 	{
+		/*每次取出最小的两块木头*/
 		first = less_pre_queue.top();
 		less_pre_queue.pop();
 		second = less_pre_queue.top();
 		less_pre_queue.pop();
+
+		/*把这两块木头拼成的长木头长度重新加入优先队列中*/
 		less_pre_queue.push(first + second);
 
+		/*酬金加上这两块木头的长度*/
 		cost += first + second;
 	}
 
