cbe41fd3c92fc1dc4a909e9dc49914a94033c224
double Z <39005525+doubleZ0108@users.noreply.github.com>
2018-12-30 09:14:04 +0800

    邻接矩阵存 (AC) (猜想原因可能还是起点和终点数量和位置不定)

--- 关键活动/CriticialActivity'.cpp
+++ 关键活动/CriticialActivity'.cpp
@@ -6,8 +6,8 @@
 using namespace std;
 
 #define INFINITE 1<<30
-typedef int Vertex;
 
+typedef int Vertex;
 struct Edge
 {
 	Vertex v1, v2;
@@ -16,15 +16,14 @@ struct Edge
 	friend istream& operator>>(istream &is, const Edge &buf);
 };
 
-
 class Activity
 {
 private:
-	vector<vector<Vertex> > NetWork;
+	vector<vector<Vertex> > NetWork;			//用邻接矩阵存储网络的顶点和边
 	vector<vector<int> > result;
 
 	vector<int> Indegree, Outdegree;
-	vector<int> EarliestTime, LatestTime;
+	vector<int> EarliestTime, LatestTime;		//最早开始时间和最迟开始时间
 
 	int ECT;		//最早完成时间
 	int idx;
@@ -34,8 +33,8 @@ public:
 	Activity(int VertexNum, int EdgeNum);
 	void Init(int EdgeNum);
 
-	bool TopoSort();
-	void findCriticialPath();
+	bool TopoSort();					//正向拓扑排序用于检测调度是否可行 & 计算最早开始时间
+	void findCriticialPath();			//逆向拓扑排序计算最迟开始时间 & 时间余量
 	void showCriticialPath();
 };
 int main(void)
@@ -68,16 +67,21 @@ istream& operator>>(istream &is, Edge &buf)
 
 Activity::Activity(int VertexNum, int EdgeNum)
 {
+	/*初始化邻接矩阵*/
 	NetWork.resize(VertexNum, vector<Vertex>(VertexNum, INFINITE));
 
+	/*初始化结果集合*/
 	result.resize(NetWork.size(), vector<int>(NetWork.size(), INFINITE));
 
+	/*初始化入度和出度*/
 	Indegree.resize(NetWork.size(), 0);
 	Outdegree.resize(NetWork.size(), 0);
 
+	/*初始化最早开始时间和最迟开始时间*/
 	EarliestTime.resize(NetWork.size(), 0);
 	LatestTime.resize(NetWork.size(), INFINITE);
 
+	/*读入顶点和边 并 计算入度和出度*/
 	Init(EdgeNum);
 }
 
@@ -87,7 +91,7 @@ void Activity::Init(int EdgeNum)
 	for (int i = 0; i < EdgeNum; ++i)
 	{
 		cin >> buf;
-		NetWork[buf.v1-1][buf.v2-1] = buf.cost;
+		NetWork[buf.v1-1][buf.v2-1] = buf.cost;		//题目从1开始输入的
 	}
 
 	/*计算各顶点的入度和出度*/
@@ -110,6 +114,7 @@ bool Activity::TopoSort()
 	
 	queue<int> Q;
 	for (int i = 0; i < NetWork.size(); ++i)
+		//将所有入度为0的点压入队列
 	{
 		if (Indegree[i] == 0)
 		{
@@ -126,12 +131,13 @@ bool Activity::TopoSort()
 		{
 			if (NetWork[V][j] != INFINITE)
 			{
-				//EarliestTime[j] = max(EarliestTime[j], EarliestTime[V] + NetWork[V][j]);
 				if (EarliestTime[V] + NetWork[V][j] > EarliestTime[j])
+					//选取最长路径
 				{
 					EarliestTime[j] = EarliestTime[V] + NetWork[V][j];
 				}
 				if (--Indegree[j] == 0)
+					//将入度为0的点压入队列
 				{
 					Q.push(j);
 				}
@@ -140,20 +146,24 @@ bool Activity::TopoSort()
 	}
 
 	this->ECT = *max_element(EarliestTime.begin(), EarliestTime.end());		
-			//最早完成时间应是所欲元素中最大的
-	idx = max_element(EarliestTime.begin(), EarliestTime.end()) - EarliestTime.begin();
+			//最早完成时间应是所有元素中最大的
 
-	return (cnt == NetWork.size());
+	idx = max_element(EarliestTime.begin(), EarliestTime.end()) - EarliestTime.begin();
+			//最早完成时间的位置
+
+	return (cnt == NetWork.size());		
+			//如果所有顶点都被拓扑排序遍历过则该调度可行
 }
 
 void Activity::findCriticialPath()
 {
 	int i, j, V;
 
-	LatestTime[idx] = ECT;
+	LatestTime[idx] = ECT;			//假设最后活动的最迟开始时间等于最早开始时间
 
 	queue<int> Q;
 	for (i = 0; i < NetWork.size(); ++i)
+		//将所有出度为0的顶点压入队列
 	{
 		if (Outdegree[i] == 0)
 		{
@@ -169,14 +179,15 @@ void Activity::findCriticialPath()
 		{
 			if (NetWork[j][V] != INFINITE)
 			{
-				//LatestTime[j] = min(LatestTime[j], LatestTime[V] - NetWork[j][V]);
-
 				if (LatestTime[V] - NetWork[j][V] <= LatestTime[j])
+					//逆推时选取最小的情况
 				{
 					LatestTime[j] = LatestTime[V] - NetWork[j][V];
 					result[j][V] = LatestTime[V] - EarliestTime[j] - NetWork[j][V];
+							//计算时间余量
 				}
 				if (--Outdegree[j] == 0)
+					//将出度为0的顶点压入队列
 				{
 					Q.push(j);
 				}
@@ -187,12 +198,15 @@ void Activity::findCriticialPath()
 
 void Activity::showCriticialPath()
 {
-	cout << ECT << endl;
+	cout << ECT << endl;			//输出整个项目所需的最少完成时间
+
 	for (int i = 0; i < NetWork.size(); ++i)
 	{
 		for (int j = NetWork.size() - 1; j >= 0; --j)
+			//按照题目要求, 第一个顶点相同时逆向输出边
 		{
 			if (result[i][j] == 0)
+				//时间余量为0的活动为关键活动
 			{
 				cout << i + 1 << "->" << j + 1 << endl;
 			}
