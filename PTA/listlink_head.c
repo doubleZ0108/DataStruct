134fa10f6219eece67e6292371e3abf115f4da84
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-02-04 11:28:11 +0800

    PTA: 带头结点的链式表操作集

---
+++ PTA/listlink_head.c
@@ -0,0 +1,147 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ERROR NULL
+typedef enum { false, true } bool;
+typedef int ElementType;
+typedef struct LNode *PtrToLNode;
+struct LNode {
+	ElementType Data;
+	PtrToLNode Next;
+};
+typedef PtrToLNode Position;
+typedef PtrToLNode List;
+
+List MakeEmpty();
+Position Find(List L, ElementType X);
+bool Insert(List L, ElementType X, Position P);
+bool Delete(List L, Position P);
+
+int main()
+{
+	List L;
+	ElementType X;
+	Position P;
+	int N;
+	bool flag;
+
+	L = MakeEmpty();
+	scanf("%d", &N);
+	while (N--) {
+		scanf("%d", &X);
+		flag = Insert(L, X, L->Next);
+		if (flag == false) printf("Wrong Answer\n");
+	}
+	scanf("%d", &N);
+	while (N--) {
+		scanf("%d", &X);
+		P = Find(L, X);
+		if (P == ERROR)
+			printf("Finding Error: %d is not in.\n", X);
+		else {
+			flag = Delete(L, P);
+			printf("%d is found and deleted.\n", X);
+			if (flag == false)
+				printf("Wrong Answer.\n");
+		}
+	}
+	flag = Insert(L, X, NULL);
+	if (flag == false) printf("Wrong Answer\n");
+	else
+		printf("%d is inserted as the last element.\n", X);
+	P = (Position)malloc(sizeof(struct LNode));
+	flag = Insert(L, X, P);
+	if (flag == true) printf("Wrong Answer\n");
+	flag = Delete(L, P);
+	if (flag == true) printf("Wrong Answer\n");
+	for (P = L->Next; P; P = P->Next) printf("%d ", P->Data);
+
+	system("pause");
+	return 0;
+}
+
+/* 你的代码将被嵌在这里 */
+List MakeEmpty()
+{
+	List head = malloc(sizeof(*head));
+	head->Next = NULL;
+
+	return head;
+}
+Position Find(List L, ElementType X)
+{
+	if (L == NULL || L->Next==NULL) { return ERROR; }
+	PtrToLNode move = L->Next;
+
+	while (move != NULL)
+	{
+		if (move->Data == X) { return move; }
+		move = move->Next;
+	}
+
+	return ERROR;
+}
+bool Insert(List L, ElementType X, Position P)
+{
+	if (L == NULL) { printf("Wrong Position for Insertion\n"); return false; }
+	else if (L->Next==NULL && P!=NULL){ printf("Wrong Position for Insertion\n"); return false; }
+	else if (L->Next == NULL && P == NULL)
+	{
+		PtrToLNode fresh = malloc(sizeof(*fresh));
+		fresh->Data = X;
+		fresh->Next = NULL;
+		L->Next = fresh;
+
+		return true;
+	}
+
+	PtrToLNode move = L;
+	while (move->Next != NULL)
+	{
+		if (move->Next == P)
+		{
+			PtrToLNode fresh = malloc(sizeof(*fresh));
+			fresh->Data = X;
+			fresh->Next = move->Next;
+			move->Next = fresh;
+
+			return true;
+		}
+		move = move->Next;
+	}
+
+	if (P == NULL)
+	{
+		PtrToLNode fresh = malloc(sizeof(*fresh));
+		fresh->Data = X;
+		fresh->Next = NULL;
+		move->Next = fresh;
+		return true;
+	}
+
+	printf("Wrong Position for Insertion\n"); 
+	return false;
+}
+bool Delete(List L, Position P)
+{
+	if (L == NULL || P == NULL) { printf("Wrong Position for Deletion\n"); return false; }
+	else if (L->Next==NULL && P==L){ printf("Wrong Position for Deletion\n"); return false; }
+
+	PtrToLNode move = L;
+	while (move->Next != NULL)
+	{
+		if (move->Next == P)
+		{
+			PtrToLNode save = move->Next;
+			move->Next = move->Next->Next;
+			free(save); save = NULL;
+			
+			return true;
+		}
+		move = move->Next;
+	}
+
+	printf("Wrong Position for Deletion\n"); 
+	return false;
+}
