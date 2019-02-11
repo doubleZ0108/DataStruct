ef670dff666cdba7c451a277206c3bb071d04157
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-02-11 14:37:12 +0800

    PTA: 求树高度 & 输出叶结点 & 前中后序 & 循环队列实现层次遍历

--- PTA/BinTree.c
+++ PTA/BinTree.c
@@ -13,6 +13,10 @@ struct TNode{
 BinTree CreatBinTree(); /* 实现细节忽略 */
 int GetHeight( BinTree BT );    /*求二叉树的高度*/
 void PreorderPrintLeaves( BinTree BT )  /*先序输出叶结点*/
+void InorderTraversal( BinTree BT );
+void PreorderTraversal( BinTree BT );
+void PostorderTraversal( BinTree BT );
+void LevelorderTraversal( BinTree BT );
 
 int main()
 {
@@ -41,4 +45,56 @@ void PreorderPrintLeaves( BinTree BT )
     PreorderPrintLeaves(BT->Left);
     PreorderPrintLeaves(BT->Right);
   }
+}
+
+void InorderTraversal( BinTree BT )
+{
+  if(BT)
+  {
+    InorderTraversal(BT->Left);
+    printf (" %c",BT->Data);
+    InorderTraversal(BT->Right);
+  }
+}
+void PreorderTraversal( BinTree BT )
+{
+  if(BT)
+  {
+    printf (" %c",BT->Data);
+    PreorderTraversal(BT->Left);
+    PreorderTraversal(BT->Right);
+  }
+}
+void PostorderTraversal( BinTree BT )
+{
+  if(BT)
+  {
+    PostorderTraversal(BT->Left);
+    PostorderTraversal(BT->Right);
+    printf (" %c",BT->Data);
+  }
+}
+void LevelorderTraversal( BinTree BT )
+{
+  if(!BT){return;}
+  
+#define MAXSIZE 1000
+  BinTree queue[MAXSIZE]={BT};  //将根压入队列中
+  int front=0, rear=MAXSIZE-1;
+  
+  BinTree move;
+  while(rear!= front)    //队列不空
+  {
+    /*出队列*/
+    rear=(rear+1)%MAXSIZE;
+    move=queue[rear];
+    
+    /*访问*/
+    printf (" %c",move->Data);
+    
+    /*左右孩子入队*/
+    if(move->Left){front=(front+1)%MAXSIZE; queue[front]=move->Left;}
+    if(move->Right){front=(front+1)%MAXSIZE; queue[front]=move->Right;}
+  }
+  
 }
