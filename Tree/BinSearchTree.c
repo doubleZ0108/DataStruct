763e50b7b8e2178ec5f76f8d709089ec88920d12
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-02-12 09:51:34 +0800

    二叉搜索树: 查找 找最大/小元素

--- Tree/BinSearchTree.c
+++ Tree/BinSearchTree.c
@@ -27,3 +27,26 @@ Position Find_nonRecursive(BinTree BST, ElementType X)
 
 	return NULL;
 }
+
+
+/*二叉搜索树中最小的元素即为树中最左边的元素*/
+Position FindMin(BinTree BST)
+{
+	if (BST)	//空树
+		{ return NULL; }
+	else if	(!BST->leftChild) //左孩子为空 => 此节点是最左的结点了
+		{ return BST; }
+	else 
+		{ return FindMin(BST->leftChild); }
+}
+
+Position FindMax(BinTree BST)
+{
+	if (BST)	//将判空与往右找的过程合二为一
+	{
+		while (BST->rightChild) 
+			{ BST = BST->rightChild; }
+	}
+
+	return BST;
+}
