59a35fa6682b19415aa5cbf67703815f4f36bdc7
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-02-15 16:10:55 +0800

    最大堆 (插入)

--- Tree/MaxHeap.c
+++ Tree/MaxHeap.c
@@ -20,3 +20,20 @@ MaxHeap MakeMaxHeap(int MaxSize)
 	H->Elements[0] = INFINITE;		//0位置设立哨兵, 其值为最大数字
 	return H;
 }
+
+void Insert(MaxHeap H, ElementType item)
+{
+	if (isFull(H))
+	{
+		printf("堆已满!\n");
+		return;
+	}
+
+	int i = ++H->size;		//1.将H的元素个数加一个		 2.用i指示最后一个元素的位置
+
+	for (; H->Elements[i / 2] < item; i /= 2)	//从父节点开始一直往上比较,如果上面小,就把父亲往下挪一层
+	{//因为元素是从1号位置开始存的,所以正常i>1也要作为循环条件,而加入哨兵的意义就在此,跟哨兵比循环一定退出
+		H->Elements[i] = H->Elements[i / 2];
+	}
+	H->Elements[i] = item;
+}
