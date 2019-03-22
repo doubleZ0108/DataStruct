51d141a6ec03c67711ca367114394618e1ed7d96
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-03-22 21:17:45 +0800

    插入排序: 稳定

--- Sort/Insertion_Sort.cpp
+++ Sort/Insertion_Sort.cpp
@@ -6,8 +6,7 @@ void Insertion_Sort(ElementType arr[], int N)
 	for (i = 1; i < N; ++i)
 	{
 		tmp = arr[i];
-		for (j = i; j > 0 && arr[j - 1] > tmp; --j)		//这里是跟tmp比较, 不是冒泡那种相邻的两个比较
-			//这里不用写>=0, 外层循环都没考虑0号位置
+		for (j = i; j >= 0 && arr[j - 1] > tmp; --j)		//这里是跟tmp比较, 不是冒泡那种相邻的两个比较
 		{
 			arr[j] = arr[j - 1];
 		}
